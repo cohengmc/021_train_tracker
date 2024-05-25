60d68c86e231615fe09613b322fcc7d5407c58cb
Geoffrey <you@example.comcohengmc@gmail.com>
2024-05-25 11:03:52 -0400

    changes

---
+++ Page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+export default function Page() {
+  return (
+    <div>
+
+      <Nav />
+      <img id="usa-map" src="./images/USA_MAP.png" alt="USA Map" />
+      
+    </div>
+  );
+}
---
+++ components/Nav.jsx
@@ -0,0 +1,19 @@
+export default function Nav() {
+  return (
+    <div className="navbar">
+      <div id="logo-area">
+        <img id="logo" src="./images/Train.gif" alt="Train animation logo" />
+        <h1>Train Tracker</h1>
+      </div>
+
+      <ul id="navOptions">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Github</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
